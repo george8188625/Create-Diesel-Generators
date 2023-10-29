ecdc09cdfd37384079ed6539627d014017a12f40
george8188625 <135385317+george8188625@users.noreply.github.com>
2023-10-29 21:31:38 +0100

    version 1.2b of the mod

--- src/main/java/com/jesz/createdieselgenerators/compat/computercraft/CCProxy.java
+++ src/main/java/com/jesz/createdieselgenerators/compat/computercraft/CCProxy.java
@@ -22,6 +22,8 @@ public class CCProxy {
     private static Function<SmartBlockEntity, ? extends AbstractComputerBehaviour> computerFactory;
 
     public static AbstractComputerBehaviour behaviour(SmartBlockEntity sbe) {
+        fallbackFactory = FallbackComputerBehaviour::new;
+
         if (computerFactory == null)
             return fallbackFactory.apply(sbe);
         return computerFactory.apply(sbe);
