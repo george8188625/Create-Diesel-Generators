8ea500fb9a9cd75b602fd9688b685c5f6506627a
george8188625 <135385317+george8188625@users.noreply.github.com>
2025-08-12 14:14:13 +0200

    fix canister fluid duplication glitch

--- src/main/java/com/jesz/createdieselgenerators/content/tools/FueledToolItem.java
+++ src/main/java/com/jesz/createdieselgenerators/content/tools/FueledToolItem.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
 import net.minecraftforge.fml.ModList;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.function.BiConsumer;
@@ -19,26 +20,32 @@ public interface FueledToolItem {
     default int getBaseCapacity(ItemStack stack){
         return CDGConfig.TOOL_CAPACITY.get();
     }
+
     default int getCapacityEnchantmentAddition(ItemStack stack){
         return CDGConfig.TOOL_CAPACITY_ENCHANTMENT.get();
     }
-    default int getCapacity(ItemStack stack){
-        if(!ModList.get().isLoaded("dungeons_libraries"))
+
+    default int getCapacity(ItemStack stack) {
+        if (!ModList.get().isLoaded("dungeons_libraries"))
             return getBaseCapacity(stack) + (getCapacityEnchantmentAddition(stack) * stack.getEnchantmentLevel(AllEnchantments.CAPACITY.get()));
         return getBaseCapacity(stack);
 
     }
+
     default FluidStack readFluid(ItemStack stack){
         return FluidStack.loadFluidStackFromNBT(stack.getOrCreateTag().getCompound("Fluid"));
     }
+
     default void writeFluid(ItemStack stack, FluidStack fluid){
         stack.getOrCreateTag().put("Fluid", fluid.writeToNBT(new CompoundTag()));
     }
+
     default int getCurrentFillLevel(ItemStack stack){
         return readFluid(stack).getAmount();
     }
-    default void createTooltip(List<Component> tooltip, ItemStack stack){
-        if(stack.getTag() != null) {
+
+    default void createTooltip(List<Component> tooltip, ItemStack stack) {
+        if (stack.getTag() != null) {
             FluidStack fluid = readFluid(stack);
             if(fluid.isEmpty()){
                 tooltip.add(Component.translatable("createdieselgenerators.tooltip.empty").withStyle(ChatFormatting.GRAY));
@@ -56,10 +63,12 @@ public interface FueledToolItem {
         }
         tooltip.add(Component.translatable("createdieselgenerators.tooltip.empty").withStyle(ChatFormatting.GRAY));
     }
-    default FluidHandlerItemStack getFluidHandler(ItemStack stack){
+
+    default FluidHandlerItemStack getFluidHandler(ItemStack stack) {
         return new ToolItemFluidHandler(stack, getCapacity(stack), this::readFluid, this::writeFluid);
     }
-    class ToolItemFluidHandler extends FluidHandlerItemStack{
+
+    class ToolItemFluidHandler extends FluidHandlerItemStack {
         BiConsumer<ItemStack, FluidStack> write;
         Function<ItemStack, FluidStack> read;
         public ToolItemFluidHandler(ItemStack container, int capacity, Function<ItemStack, FluidStack> read, BiConsumer<ItemStack, FluidStack> write) {
@@ -71,6 +80,13 @@ public interface FueledToolItem {
         public FluidStack getFluid() {
             return read.apply(container);
         }
+
+        @Override
+        protected void setContainerToEmpty() {
+            write.accept(container, FluidStack.EMPTY);
+
+        }
+
         @Override
         protected void setFluid(FluidStack fluid) {
             write.accept(container, fluid);
